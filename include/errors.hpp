40ba0d66a155075dbd4f17ff4e7e8c721c7d28c4
Nebojša Koturović <necer95@gmail.com>
2020-07-20 19:11:08 +0200

    Small fixes in errors file

--- include/errors.hpp
+++ include/errors.hpp
@@ -5,7 +5,6 @@
 
 namespace rs {
 
-
 class Exception : std::exception {
 protected:
     Exception(json_t &&info) : info(std::move(info)) {}
@@ -13,9 +12,9 @@ public:
     json_t info;
 
     virtual ~Exception() = default;
-    virtual const char * id() const = 0;
-    virtual const char * msg() const = 0;
-    virtual restinio::http_status_line_t status() const = 0;
+    constexpr virtual const char * id() const = 0;
+    constexpr virtual const char * msg() const = 0;
+    constexpr virtual restinio::http_status_line_t status() const = 0;
     const char * what() const noexcept override {
         return this->msg();
     }
@@ -35,43 +34,43 @@ public:
 template<typename C>
 concept CException = std::derived_from<C,Exception>;
 
-class Error : public Exception {
+class Error final : public Exception {
 public:
     Error(json_t &&info = {}) : Exception(std::move(info)) {}
-    const char * id() const override { return "OtherError"; }
-    const char * msg() const override { return "Other error"; }
+    constexpr const char * id() const override { return "OtherError"; }
+    constexpr const char * msg() const override { return "Other error"; }
     restinio::http_status_line_t status() const override { return restinio::status_internal_server_error(); }
 };
 
-class InvalidParamsError : public Exception {
+class InvalidParamsError final : public Exception {
 public:
     InvalidParamsError(json_t &&info = {}) : Exception(std::move(info)) {}
-    const char * id() const override { return "InvalidParams"; }
-    const char * msg() const override { return "Invalid Parameters"; }
+    constexpr const char * id() const override { return "InvalidParams"; }
+    constexpr const char * msg() const override { return "Invalid Parameters"; }
     restinio::http_status_line_t status() const override { return restinio::status_bad_request(); }
 };
 
-class JsonParseError : public Exception {
+class JsonParseError final : public Exception {
 public:
     JsonParseError(json_t &&info = {}) : Exception(std::move(info)) {}
-    const char * id() const override { return "JsonParseError"; }
-    const char * msg() const override { return "Error parsing JSON"; }
+    constexpr const char * id() const override { return "JsonParseError"; }
+    constexpr const char * msg() const override { return "Error parsing JSON"; }
     restinio::http_status_line_t status() const override { return restinio::status_bad_request(); }
 };
 
-class NotFoundError : public Exception {
+class NotFoundError final : public Exception {
 public:
     NotFoundError(json_t &&info = {}) : Exception(std::move(info)) {}
-    const char * id() const override { return "NotFound"; }
-    const char * msg() const override { return "Resource not found"; }
+    constexpr const char * id() const override { return "NotFound"; }
+    constexpr const char * msg() const override { return "Resource not found"; }
     restinio::http_status_line_t status() const override { return restinio::status_not_found(); }
 };
 
-class DBError : public Exception {
+class DBError final : public Exception {
 public:
     DBError(json_t &&info = {}) : Exception(std::move(info)) {}
-    const char * id() const override { return "DBError"; }
-    const char * msg() const override { return "Database Error"; }
+    constexpr const char * id() const override { return "DBError"; }
+    constexpr const char * msg() const override { return "Database Error"; }
     restinio::http_status_line_t status() const override { return restinio::status_internal_server_error(); }
 };
 
